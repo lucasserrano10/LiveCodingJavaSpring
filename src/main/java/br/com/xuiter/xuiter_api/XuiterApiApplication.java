19b4f3d432b8fb625289fb075d7a720d24c8b95b
Lucas Serrano Rocco <156000513+lucasserrano10@users.noreply.github.com>
2025-10-06 17:51:17 -0300

    initial setup

---
+++ src/main/java/br/com/xuiter/xuiter_api/XuiterApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.xuiter.xuiter_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuiterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XuiterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/xuiter/xuiter_api/XuiterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xuiter.xuiter_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuiterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
